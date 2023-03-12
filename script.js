9f4f9a70b64ee55160461acb4ee21430913c64ad
PremsinghP <premsinghpadya944@gmail.com>
2023-03-12 13:04:51 +0530

    second commit

--- script.js
+++ script.js
@@ -13,22 +13,21 @@ function generateResume() {
   doc.setFontSize(20);
   doc.text(name, 15, 15);
   doc.setFontSize(14);
-  doc.text(email, 15, 25);
-  doc.text(phone, 15, 30);
+  doc.text(email + " | " + phone, 15, 25);
   doc.text(address, 15, 35);
   doc.setFontSize(16);
-  doc.text("Summary", 15, 50);
-  doc.setFontSize(12);
-  doc.text(summary, 15, 55);
+  doc.text("Summary", 15, 60);
+  doc.setFontSize(14);
+  doc.text(summary, 15, 70);
   doc.setFontSize(16);
-  doc.text("Experience", 15, 80);
-  doc.setFontSize(12);
-  doc.text(experience, 15, 85);
+  doc.text("Experience", 15, 95);
+  doc.setFontSize(14);
+  doc.text(experience, 15, 105);
   doc.setFontSize(16);
-  doc.text("Education", 15, 120);
-  doc.setFontSize(12);
-  doc.text(education, 15, 125);
+  doc.text("Education", 15, 130);
+  doc.setFontSize(14);
+  doc.text(education, 15, 140);
 
-  // Save PDF
-  doc.save(name + "_resume.pdf");
+  // Save PDF document
+  doc.save(name + "-resume.pdf");
 }
